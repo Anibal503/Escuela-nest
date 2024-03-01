5022b74c3ccc77ab89b7f24c6dde593ab4555219
Anibal <mb232100239@uma.edu.sv>
2024-02-29 19:39:46 -0600

    Agregando funcion suma

--- src/app.service.ts
+++ src/app.service.ts
@@ -11,4 +11,9 @@ getGreeting(): string {
   return "Greetings, programmer!";
 }
 
+sumar(operador1, operador2): number{
+  let resultado = 0;
+  resultado = parseInt(operador1) + parseInt(operador2);
+  return resultado;
+}
 }
